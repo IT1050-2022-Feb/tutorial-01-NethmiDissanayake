913eec6f862bc827a02a99adfd5edd9fee67ad82
IT21185748 <97587666+NethmiDissanayake@users.noreply.github.com>
2022-02-13 04:20:42 +0000

    Activity 1

--- Tute04.c
+++ Tute04.c
@@ -5,26 +5,41 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
- int min (int no1, int no2);
- int max (int no1, int no2);
- int multi (int no1, int no2);
+int mnimum(int , int );
+int maximum(int , int );
+int multiply(int , int );
 
 int main() {
-   int no1, no2, minimum, maximum, multiply;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
+  int no1, no2;
+  printf("Enter a value for no 1 : ");
+  scanf("%d", &no1);
+  printf("Enter a value for no 2 : ");
+  scanf("%d", &no2);
+  printf("%d ", mnimum(no1, no2));
+  printf("%d ", maximum(no1, no2));
+  printf("%d ", multiply(no1, no2));
 
-   minimum = min(no1, no2);
-   maximum = max(no1, no2);
+  return 0;
 
-   printf("%d ", minimum);
-   printf("%d ", maximum);
-   printf("%d ", multiply);
-   return 0;
+}int mnimum(int a , int b){
+  int min;
+  if (a > b){
+   return b;
+  }else {
+   return a;
+  }
+}
 
-}int minimum (int no1, int no2){
-  return (no1 > no2) ? no2 : no1;
+int maximum(int a , int b){
+  if (a > b){
+   return a;
+  }else {
+   return b;
+  }
+}
+
+int multiply(int a, int b){
+  int mul;
+  return a * b;
 }
  
